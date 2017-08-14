be807fea30f9a3ebc3cd19fce34967ffcc443a70
Russell Osborne <russell@theironyard.com>
2017-08-14 15:04:01 -0400

    Correct test examples to return promise from supertest

--- server/tests/routes/homepage.test.js
+++ server/tests/routes/homepage.test.js
@@ -4,11 +4,16 @@ const request = require("supertest");
 describe("Homepage router", () => {
   describe("GET /", () => {
     it("will return a status code of 200", () => {
-      request(app)
+      return request(app)
       .get("/")
       .expect(200)
+    });
+
+    it("will return a performance data in the body", () => {
+      return request(app)
+      .get("/")
       .expect((req) => {
-        req.text.includes("This page was rendered")
+        req.text.includes("This page was rendered in")
       });
     });
   });
